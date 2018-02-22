c22d3324326af881f1862bcab2bbe2fdd371c1e2
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-02-22 16:32:24 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int count=0,i;
+    printf("enter the string \n");
+    scanf("%c",&a);
+     for(i=0;i<strlen(a);i++)
+     {
+         if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+     
+    {
+        count++;
+        
+    }
+    if(count>0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    }
+}
