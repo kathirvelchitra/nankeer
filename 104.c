3fd46df19b728a43fe9bd7d4de7c819b37298779
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-03-16 09:19:39 +0000

    Create 104.c

---
+++ 104.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+void main()
+{
+int n,k,s;
+printf("\n enter the N value:");
+scanf("%d",&n);
+printf("\n enter the K value:");
+scanf("%d",&k);
+s=pow(n,k);
+printf("\n %d",s);
+}
+
+
+
